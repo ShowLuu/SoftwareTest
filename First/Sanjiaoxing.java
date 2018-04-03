9f4e64f756f28f9f2634428ac1df0ce323cbd2cc
ShowLuu <1309617271@qq.com>
2018-04-03 11:49:37 +0800

    first commit

---
+++ First/Sanjiaoxing.java
@@ -0,0 +1,22 @@
+package test;
+
+import java.util.Scanner;
+
+public class Sanjiaoxing {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a,b,c;
+		Scanner keyboard=new Scanner(System.in);    //输入三角形三条边
+	     System.out.print("please input a=");
+		a=keyboard.nextDouble();
+		System.out.print("please input b=");
+		b=keyboard.nextDouble();
+		System.out.print("please input c=");
+		c=keyboard.nextDouble();
+       
+	}
+}
