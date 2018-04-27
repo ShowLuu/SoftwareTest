f7936b65384edfa039f9848313cb2100f1f5ce42
ShowLuu <1309617271@qq.com>
2018-04-27 20:34:31 +0800

    q

---
+++ Six/JiSuan.java
@@ -0,0 +1,28 @@
+package Test;
+
+public class JiSuan {
+	
+	public int add(int a,int b){
+		return a+b;
+	}
+	
+	public int sub(int a,int b){
+		return a-b;
+	}
+	
+	public int mul(int a,int b){
+		return a*b;
+	}
+	
+	public int div(int a,int b){
+		return a/b;
+	}
+	
+	public int num(int s){
+		if(s<0){
+			s=-s;
+		}
+		return s;
+	}
+	
+}
